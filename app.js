8c5a0915225adbceac718ec4a9cbaed712bfd9ab
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-09 23:52:53 +0200

    render user list and user info by click for user

--- app.js
+++ app.js
@@ -49,3 +49,103 @@ btn.addEventListener("click", (e) => {
 
   getPosts(renderPosts);
 });
+
+(function () {
+  const apiUrl = "https://jsonplaceholder.typicode.com";
+  const usersListEl = document.querySelector(".users-list");
+  const userInfoEl = document.querySelector(".user-info");
+
+  usersListEl.addEventListener("click", onUserClickHandler);
+
+  function onUserClickHandler(e) {
+    e.preventDefault();
+
+    if (e.target.dataset.userId) {
+      const userID = e.target.dataset.userId;
+      getUserInfoHTTP(userID, onGetUserInfoCallback);
+    }
+  }
+
+  function getUsersHttp(cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("get", `${apiUrl}/users`);
+    xhr.addEventListener("load", () => {
+      if (xhr.status !== 200) {
+        console.log("Error", xhr.status);
+        return;
+      }
+
+      const res = JSON.parse(xhr.responseText);
+      cb(res);
+    });
+    xhr.send();
+  }
+
+  function getUserInfoHTTP(id, cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("get", `${apiUrl}/users/${id}`);
+    xhr.addEventListener("load", () => {
+      if (xhr.status !== 200) {
+        console.log("Error", xhr.status);
+        return;
+      }
+
+      const res = JSON.parse(xhr.responseText);
+      cb(res);
+    });
+    xhr.send();
+  }
+
+  function onGetUserInfoCallback(user) {
+    if (!user.id) {
+      console.log("User not found");
+      return;
+    }
+    renderUserInfo(user);
+  }
+
+  function renderUserInfo(user) {
+    userInfoEl.innerHTML = "";
+    const template = userInfoTemplate(user);
+    userInfoEl.insertAdjacentHTML("afterbegin", template);
+  }
+
+  function onGetUsersCallback(users) {
+    if (!users.length) {
+      return;
+    }
+
+    renderUsersList(users);
+  }
+
+  function renderUsersList(users) {
+    const fragment = users.reduce(
+      (acc, user) => acc + userListItemTemplate(user),
+      ""
+    );
+    usersListEl.insertAdjacentHTML("afterbegin", fragment);
+  }
+
+  function userListItemTemplate(user) {
+    return `
+    <button type="button" class="list-group-item list-group-item-action" data-user-id="${user.id}">${user.name}</button>
+    `;
+  }
+
+  function userInfoTemplate(user) {
+    return `<div class="card border-dark mb-3">
+    <div class="card-header">${user.name}</div>
+    <div class="card-body text-dark">
+      <ul class="list-group list-group-flush">
+      <li class='list-group-item'>${user.username}</li>
+      <li class='list-group-item'>${user.website}</li>
+      <li class='list-group-item'>${user.company.name}</li>
+      <li class='list-group-item'>${user.address.city}</li>
+      </ul>
+    </div>
+    <fiv class='card-footer border-dark'>Phone: ${user.phone}</fiv>
+  </div>`;
+  }
+
+  getUsersHttp(onGetUsersCallback);
+})();
