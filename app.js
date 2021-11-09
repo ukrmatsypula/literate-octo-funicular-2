c1171db5677cb4c2d35a09484fe46845ed1629c5
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-10 00:23:26 +0200

    added new post and render it by ajax

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const btn = document.querySelector("#btn");
+const btnGetPosts = document.querySelector(".btn-get-posts");
+const btnCreatePost = document.querySelector(".btn-create-post");
+
 const container = document.querySelector(".container");
 
 function getPosts(cb) {
@@ -15,39 +17,72 @@ function getPosts(cb) {
   xhr.send();
 }
 
+function createNewPost(body, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+  xhr.addEventListener("load", () => {
+    const response = JSON.parse(xhr.responseText);
+    cb(response);
+  });
+
+  xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+
+  xhr.addEventListener("error", () => {
+    console.log("error");
+  });
+  xhr.send(JSON.stringify(body));
+}
+
+function cardTemplate(post) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  const title = document.createElement("h5");
+  title.classList.add("card-title");
+  title.textContent = post.title;
+
+  const article = document.createElement("p");
+  article.classList.add("card-text");
+  article.textContent = post.body;
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(article);
+
+  card.appendChild(cardBody);
+  return card;
+}
+
 function renderPosts(response) {
   const fragment = document.createDocumentFragment();
   response.forEach((post) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-
-    const title = document.createElement("h5");
-    title.classList.add("card-title");
-    title.textContent = post.title;
-
-    const article = document.createElement("p");
-    article.classList.add("card-text");
-    article.textContent = post.body;
-
-    cardBody.appendChild(title);
-    cardBody.appendChild(article);
-
-    card.appendChild(cardBody);
+    const card = cardTemplate(post);
     fragment.appendChild(card);
-
-    return card;
   });
 
   container.appendChild(fragment);
 }
 
-btn.addEventListener("click", (e) => {
+btnGetPosts.addEventListener("click", (e) => {
   e.preventDefault();
 
   getPosts(renderPosts);
+});
+
+btnCreatePost.addEventListener("click", () => {
+  const newPost = {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  };
+
+  createNewPost(newPost, (response) => {
+    const card = cardTemplate(response);
+    container.insertAdjacentElement("afterend", card);
+  });
 });
 
 (function () {
