38ac4f046b854730c20f98073d24beeb26617e8c
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-11 23:48:49 +0200

    added new changes

--- app.js
+++ app.js
@@ -184,3 +184,109 @@ btnCreatePost.addEventListener("click", () => {
 
   getUsersHttp(onGetUsersCallback);
 })();
+
+function myHttpRequest({ method, url } = {}, cb) {
+  try {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.addEventListener("load", () => {
+      if (Math.floor(xhr.status / 100) !== 2) {
+        cb(`Error. Status code: ${xhr.status}`, xhr);
+        return;
+      }
+      const response = JSON.parse(xhr.responseText);
+      cb(null, response);
+    });
+
+    xhr.addEventListener("error", () => {
+      cb(`Error. Status code: ${xhr.status}`, xhr);
+    });
+    xhr.send();
+  } catch (error) {
+    cb(error);
+  }
+}
+
+// myHttpRequest(
+//   { method: "GET", url: "https://jsonplaceholder.typicode.com/posts" },
+//   (err, response) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log(response);
+//   }
+// );
+
+function http() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.addEventListener("load", () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener("error", () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
+    },
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.addEventListener("load", () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener("error", () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value)
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
+    },
+  };
+}
+
+const myHttp = http();
+
+myHttp.post(
+  "https://jsonplaceholder.typicode.com/posts",
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  },
+  {
+    "Content-type": "application/json",
+    "x-auth": "sakgjf32j20fj20jfij02fei0j0fjdf",
+  },
+  (err, response) => {
+    console.log(err, response);
+  }
+);
