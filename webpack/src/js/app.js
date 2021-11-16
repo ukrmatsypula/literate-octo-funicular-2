62a6c8ae7375629d78963bbb66ef6b559d758938
Roman Matsypula <ukrmatsypula@gmail.com>
2021-11-16 16:58:29 +0200

    added protection

--- webpack/src/js/app.js
+++ webpack/src/js/app.js
@@ -9,4 +9,40 @@ import Product from "./module2";
 
 const product = new Product("Yellow");
 
-console.log(product)
+console.log(product);
+
+// const User = {
+//   name: "Denis",
+//   getName() {
+//     return this.name;
+//   },
+//   setName(name) {
+//     this.name = name;
+//   },
+// };
+
+// User.setName('Roman');
+// console.log(User.name);
+
+// function User(name) {
+//   let userName = name;
+
+//   return Object.freeze({
+//     getName() {
+//       return userName;
+//     },
+//     setName(name) {
+//       userName = name;
+//     },
+//   });
+// }
+
+// const denis = new User("Roman");
+
+let data = {
+  name: "Denis",
+};
+
+export function getData() {
+  return data;
+}
